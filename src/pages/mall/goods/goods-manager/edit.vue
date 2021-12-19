c021629030b29d4b59594bc52e373e367d51ba26
wangyuan <2814579279@qq.com>
2021-12-19 22:31:49 +0800

    fix: 修复初始金额bug

--- src/pages/mall/goods/goods-manager/edit.vue
+++ src/pages/mall/goods/goods-manager/edit.vue
@@ -7,78 +7,47 @@
 -->
 <template>
   <div class="m20 p20 bg-white">
-
     <el-form
       label-position="right"
       label-width="120px"
       :model="form"
       ref="form"
-      size='small'
+      size="small"
       class="mb50"
     >
-      <el-form-item
-        label="商品名："
-        prop="name"
-        :verify="{maxLen: 30}"
-      >
-        <el-input
-          v-model="form.name"
-          style="width:500px"
-        ></el-input>
+      <el-form-item label="商品名：" prop="name" :verify="{ maxLen: 30 }">
+        <el-input v-model="form.name" style="width: 500px"></el-input>
         <div class="f12 f-grey">商品名称，建议30字以内</div>
       </el-form-item>
-      <el-form-item
-        label="商品描述："
-        prop="describe"
-        :verify="{maxLen: 60}"
-      >
-        <el-input
-          v-model="form.describe"
-          style="width:500px"
-        ></el-input>
-        <div class="f12 f-grey">在商品详情页标题下面展示卖点信息，建议60字以内</div>
+      <el-form-item label="商品描述：" prop="describe" :verify="{ maxLen: 60 }">
+        <el-input v-model="form.describe" style="width: 500px"></el-input>
+        <div class="f12 f-grey">
+          在商品详情页标题下面展示卖点信息，建议60字以内
+        </div>
       </el-form-item>
-      <el-form-item
-        label="商品封面："
-        prop="cover"
-        verify
-      >
-        <Imgpond
-          v-model="form.cover"
-          :count="1"
-        />
+      <el-form-item label="商品封面：" prop="cover" verify>
+        <Imgpond v-model="form.cover" :count="1" />
         <div class="f12 f-grey">建议尺寸：800*800像素</div>
       </el-form-item>
-      <el-form-item
-        label="商品图："
-        prop="imgList"
-        verify
-      >
-        <Imgpond
-          v-model="form.imgList"
-          :count="10"
-        />
-        <div class="f12 f-grey">建议尺寸：800*800像素，你可以拖拽图片调整顺序，最多上传10张</div>
+      <el-form-item label="商品图：" prop="imgList" verify>
+        <Imgpond v-model="form.imgList" :count="10" />
+        <div class="f12 f-grey">
+          建议尺寸：800*800像素，你可以拖拽图片调整顺序，最多上传10张
+        </div>
       </el-form-item>
       <el-form-item
         label="价格："
         prop="price"
-        :verify="{gte:0,type:'数字'}"
+        :verify="{ gte: 0, type: '数字' }"
       >
-        <el-input
-          v-model="form.price"
-          style="width:150px"
-        ></el-input>
+        <el-input v-model="form.price" style="width: 150px"></el-input>
       </el-form-item>
       <el-form-item
         label="划线价："
         prop="originalPrice"
-        :verify="{gte:0,type:'数字'}"
+        :verify="{ gte: 0, type: '数字' }"
       >
-        <el-input
-          v-model="form.originalPrice"
-          style="width:150px"
-        ></el-input>
+        <el-input v-model="form.originalPrice" style="width: 150px"></el-input>
       </el-form-item>
       <!-- <el-form-item
         label="库存："
@@ -91,19 +60,14 @@
         ></el-input>
       </el-form-item> -->
       <el-form-item label="商品详情：">
-        <Minimce
-          style="width:800px"
-          v-model="form.richText"
-        />
+        <Minimce style="width: 800px" v-model="form.richText" />
       </el-form-item>
     </el-form>
 
     <div class="operation">
-      <el-button
-        size='small'
-        class='f-white bg-theme'
-        @click="submit()"
-      >保存商品</el-button>
+      <el-button size="small" class="f-white bg-theme" @click="submit()"
+        >保存商品</el-button
+      >
     </div>
   </div>
 </template>
@@ -116,8 +80,8 @@ export default {
   created() {
     let form = this.$route.params;
     // 价格改为元设置
-    form.price = form.price / 100;
-    form.originalPrice = form.originalPrice / 100;
+    form.price = form.price ? form.price / 100 : 0;
+    form.originalPrice = form.originalPrice ? form.originalPrice / 100 : 0;
     this.form = form;
   },
   data() {
