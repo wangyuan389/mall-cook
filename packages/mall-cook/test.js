4141fc66ccbccbb4b08f6779f50ceac69280b6b3
wangyuan389 <2814579279@qq.com>
2022-01-06 17:32:36 +0800

    feat: 重构结构

---
+++ packages/mall-cook/test.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: What's this for
+ * @Autor: WangYuan
+ * @Date: 2022-01-06 16:47:18
+ * @LastEditors: WangYuan
+ * @LastEditTime: 2022-01-06 16:49:36
+ */
+console.log('执行test11');
