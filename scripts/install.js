fc5950c4683aee21752d63709989773b336e7b49
wangyuan389 <2814579279@qq.com>
2022-01-07 16:41:25 +0800

    feat: 增加dev命令

--- scripts/install.js
+++ scripts/install.js
@@ -3,23 +3,13 @@
  * @Autor: WangYuan
  * @Date: 2022-01-06 16:13:31
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-01-06 17:29:32
+ * @LastEditTime: 2022-01-07 15:53:45
  */
-const fs = require('fs')
-const path = require('path')
+const { resolve } = require('path')
 const execa = require('execa')
-const fileDir = path.resolve(__dirname, '../packages')
 
-let paths = []
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
-
-fs.readdir(fileDir, (err, files) => {
-  if (err) {
-    console.warn(err)
-  }
-  paths = files
-})
 
 let cwd = './packages/mall-cook'
 run('yarn', ['dev'], { cwd })
