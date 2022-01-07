fd04ec3beed978dbd692c1cb621726bbd478e461
wangyuan389 <2814579279@qq.com>
2022-01-07 17:29:57 +0800

    feat: 增加运行命令

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,13 +3,15 @@
  * @Autor: WangYuan
  * @Date: 2022-01-06 16:13:31
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-01-07 15:53:45
+ * @LastEditTime: 2022-01-07 17:26:02
  */
-const { resolve } = require('path')
 const execa = require('execa')
+const { resolve } = require('path')
 
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
 
-let cwd = './packages/mall-cook'
-run('yarn', ['dev'], { cwd })
+const CWD = process.cwd()
+let PKG_MALL = resolve(CWD, './packages/mall-cook')
+
+run('yarn', ['dev'], { cwd: PKG_MALL })
--- test.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description: What's this for
- * @Autor: WangYuan
- * @Date: 2022-01-06 16:47:18
- * @LastEditors: WangYuan
- * @LastEditTime: 2022-01-06 16:47:18
- */
-console.log('执行test');
