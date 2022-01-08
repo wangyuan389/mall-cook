da393504cee82f2a56a021a04d80c4d84a747bd2
wangyuan389 <2814579279@qq.com>
2022-01-08 11:02:08 +0800

    feat: 项目结构整理完成

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,17 +1,50 @@
 /*
- * @Description: 下载子项目依赖脚本
+ * @Description: root运行子项目脚本
  * @Autor: WangYuan
  * @Date: 2022-01-06 16:13:31
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-01-07 17:26:02
+ * @LastEditTime: 2022-01-08 10:15:25
  */
 const execa = require('execa')
 const { resolve } = require('path')
+const inquirer = require('inquirer')
+
+const CWD = process.cwd()
+let PKG_PLATFORM = resolve(CWD, './packages/mall-cook-platform')
+let PKG_TEMPLATE = resolve(CWD, './packages/mall-cook-template')
 
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
 
-const CWD = process.cwd()
-let PKG_MALL = resolve(CWD, './packages/mall-cook')
+async function create () {
+  const { fruit } = await inquirer.prompt([
+    {
+      type: 'list',
+      message: '请选择您要运行的子项目:',
+      name: 'fruit',
+      choices: [
+        {
+          key: '0',
+          name: 'Mall-Cook 可视化搭建平台',
+          value: 'platform'
+        },
+        {
+          key: '1',
+          name: 'Mall-Cook H5项目',
+          value: 'h5'
+        }
+      ]
+    }
+  ])
 
-run('yarn', ['dev'], { cwd: PKG_MALL })
+  switch (fruit) {
+    case 'platform':
+      run('yarn', ['dev'], { cwd: PKG_PLATFORM })
+      break
+    case 'h5':
+      run('yarn', ['dev'], { cwd: PKG_TEMPLATE })
+      break
+  }
+}
+
+create()
