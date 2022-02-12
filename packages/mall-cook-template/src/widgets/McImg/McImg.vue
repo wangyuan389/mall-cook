7765e2597af0ce31fcd7239e830aa5c27201df0b
wangyuan389 <2814579279@qq.com>
2022-02-12 14:36:36 +0800

    fix: 修复图片物料样式

--- packages/mall-cook-template/src/widgets/McImg/McImg.vue
+++ packages/mall-cook-template/src/widgets/McImg/McImg.vue
@@ -3,15 +3,15 @@
  * @Autor: WangYuan
  * @Date: 2021-05-21 19:13:20
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-02-08 20:49:44
+ * @LastEditTime: 2022-02-12 14:34:37
 -->
 <template>
-  <div :style="[$wrapStyle(imageStyle)]">
+  <div :style="[$wrapStyle(imageStyle),$cmpStyle(this.imageStyle)]">
     <image
       class="img"
       mode="widthFix"
+      :style="[getImgStyle()]"
       :src="imageValue.imagePath || defaultImage"
-      :style="[$cmpStyle(this.imageStyle)]"
       @click="$jump(imageValue.jumpPath)"
     />
   </div>
@@ -38,6 +38,14 @@ export default {
         "https://img01.yzcdn.cn/public_files/2019/03/05/2b60ed750a93a1bd6e17fc354c86fa78.png!large.webp",
     };
   },
+
+  methods: {
+    getImgStyle(){
+      return {
+        borderRadius:`${this.imageStyle.cmpRadius}px`
+      }
+    }
+  }
 };
 </script>
 
