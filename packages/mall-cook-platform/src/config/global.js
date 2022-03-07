a53145cac3b12f66ef04c147649cc83d954ae9f5
wangyuan389 <2814579279@qq.com>
2022-03-07 16:33:10 +0800

    feat：更换预览地址

--- packages/mall-cook-platform/src/config/global.js
+++ packages/mall-cook-platform/src/config/global.js
@@ -3,11 +3,10 @@
  * @Autor: WangYuan
  * @Date: 2022-02-11 10:08:57
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-03-03 09:10:04
+ * @LastEditTime: 2022-03-07 16:25:14
  */
 export default {
-  baseApi: 'https://www.lanshan-h5.cn',
-  // baseApi:'http://110.42.184.128:180/node'
-  viewUrl: 'http://110.42.184.128:9000/#/'
+  baseApi: 'https://www.lanshan-h5.cn:1443/node/',
+  viewUrl: 'https://www.lanshan-h5.cn:1443/h5/#/'
   // viewUrl: 'http://192.168.10.70:8081/#/'
 }
