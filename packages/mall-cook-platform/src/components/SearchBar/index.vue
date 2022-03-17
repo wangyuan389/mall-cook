67749e5e43324872bdaecce6a46c49757d1448c7
wangyuan389 <2814579279@qq.com>
2022-03-17 14:56:41 +0800

    feat: 修改搜索框样式

--- packages/mall-cook-platform/src/components/SearchBar/index.vue
+++ packages/mall-cook-platform/src/components/SearchBar/index.vue
@@ -3,24 +3,29 @@
  * @Autor: WangYuan
  * @Date: 2021-06-11 13:50:32
  * @LastEditors: WangYuan
- * @LastEditTime: 2021-09-17 17:44:10
+ * @LastEditTime: 2022-03-17 14:23:25
 -->
 <template>
   <div>
     <!-- 搜索框 -->
-    <van-search
+    <!-- <van-search
       v-model="searchVal"
       show-action
       shape="round"
       placeholder="搜索商品"
     >
       <template #action>
-        <div
-          class="pl5 pr5 f-h5-theme"
-          @click="onSearch"
-        >搜索</div>
+        <div class="pl5 pr5 f-h5-theme" @click="onSearch">搜索</div>
       </template>
-    </van-search>
+    </van-search> -->
+
+    <div class="sercarh">
+      <div class="sercarh-body">
+        <i class="el-icon-search mr5"></i>
+        请输入关键词
+      </div>
+      <div class="sercarh-btn">搜索</div>
+    </div>
 
     <!-- 热门搜索 -->
     <div class="pl15 pr15 mt10 f14">
@@ -28,15 +33,13 @@
       <ul class="flex flex-wrap">
         <li
           class="hot flex-center"
-          v-for="(item,index) in project.config.hotList"
+          v-for="(item, index) in project.config.hotList"
+          :key="index"
           @click="onHot(item)"
         >
-          <img
-            src="https://b.yzcdn.cn/search-page/hot-fire.png"
-            alt=""
-          >
+          <img src="https://b.yzcdn.cn/search-page/hot-fire.png" alt="" />
           <span>
-            {{item.value}}
+            {{ item.value }}
           </span>
         </li>
       </ul>
@@ -89,6 +92,38 @@ export default {
     width: 16px;
     height: 16px;
     margin-right: 5px;
+  }
+}
+
+.sercarh {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+
+  .sercarh-body {
+    display: flex;
+    align-items: center;
+    width: 330px;
+    height: 30px;
+    margin: 0 6px 0 12px;
+    padding-left: 9px;
+    background: #f7f8fa;
+    border-radius: 15px;
+    color: #d6d6d9;
+    font-size: 13px;
+  }
+
+  .sercarh-btn {
+    width: 48px;
+    height: 26px;
+    color: #fff;
+    border-radius: 13px;
+    line-height: 26px;
+    font-size: 12px;
+    text-align: center;
+    background: linear-gradient(90deg, #ef4c2d, #d40908);
+    cursor: pointer;
   }
 }
 </style>
