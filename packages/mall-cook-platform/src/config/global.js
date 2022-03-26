61c4922a888b86774bd3b209cde2356ed71b487c
wangyuan389 <2814579279@qq.com>
2022-03-26 11:36:38 +0800

    fix：更换H5预览地址

--- packages/mall-cook-platform/src/config/global.js
+++ packages/mall-cook-platform/src/config/global.js
@@ -3,10 +3,10 @@
  * @Autor: WangYuan
  * @Date: 2022-02-11 10:08:57
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-03-21 09:25:38
+ * @LastEditTime: 2022-03-26 11:36:18
  */
 export default {
   baseApi: 'https://www.lanshan-h5.cn:1443/node/',
-  viewUrl: 'http://192.168.10.70:8081/#/'
+  viewUrl: 'https://www.lanshan-h5.cn:1443/h5/#/'
   // viewUrl: 'http://192.168.10.70:8081/#/'
 }
