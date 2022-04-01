14d26fba722a4071c58d281a41d52e83853a9e3f
hanyingbo <hanyingbo@foxmail.com>
2022-04-01 11:22:53 +0800

    chore: 移除无关内容

--- packages/mall-cook-platform/src/App.vue
+++ packages/mall-cook-platform/src/App.vue
@@ -11,13 +11,4 @@
   </div>
 </template>
 
-<script>
-export default {
-  mounted () {
-    // 隐藏加载Loading
-    document.getElementById('appLoading').style.display = 'none'
-  }
-}
-</script>
-
 <style lang="scss"></style>
