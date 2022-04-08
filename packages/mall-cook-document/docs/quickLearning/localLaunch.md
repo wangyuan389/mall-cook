b65122192d4c6758fccbb5e41f6d9b1e4776c03b
wangyuan389 <2814579279@qq.com>
2022-04-08 17:16:46 +0800

    feat:新增文档项目

---
+++ packages/mall-cook-document/docs/quickLearning/localLaunch.md
@@ -0,0 +1,14 @@
+---
+title: 本地运行
+date: 2022-04-08
+---
+
+```bash
+npm run dev
+```
+
+<p>选择需要运行的项目</p> 
+<strong><em>对应项目运行前必须安装依赖</em></strong>
+
+
+![alt 属性文本](/start.png 'xxxx')
--- packages/mall-cook-platform/src/config/global.js
+++ packages/mall-cook-platform/src/config/global.js
@@ -3,9 +3,9 @@
  * @Autor: WangYuan
  * @Date: 2022-02-11 10:08:57
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-03-26 11:44:19
+ * @LastEditTime: 2022-04-08 11:00:02
  */
 export default {
-  baseApi: 'https://www.lanshan-h5.cn:1443/node/',
-  viewUrl: 'http://110.42.184.128:9000/#/'
+  baseApi: 'https://www.lanshan-h5.cn:1443/node/',  // 后端接口地址
+  viewUrl: 'http://110.42.184.128:9000/#/'          // iframe嵌套的H5地址,本地开发可改为本地启动的H5地址
 }
