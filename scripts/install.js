dddd78d4298c29f4db5f3fb30ee9d31b20a118e7
wangyuan389 <2814579279@qq.com>
2022-08-11 14:44:49 +0800

    feat：增加service项目install

--- scripts/install.js
+++ scripts/install.js
@@ -3,28 +3,28 @@
  * @Autor: WangYuan
  * @Date: 2022-02-11 16:18:06
  * @LastEditors: WangYuan
- * @LastEditTime: 2022-08-11 14:35:48
+ * @LastEditTime: 2022-08-11 14:41:34
  */
 const execa = require('execa')
 const ora = require('ora')
 const { resolve } = require('path')
 
 const CWD = process.cwd()
-let PKG_MALL_COOK_PLATFORM = resolve(CWD, './packages/mall-cook-platform')
-let PKG_MALL_COOK_TEMPLATE = resolve(CWD, './packages/mall-cook-template')
+let PKG_PLATFORM = resolve(CWD, './packages/mall-cook-platform')
+let PKG_TEMPLATE = resolve(CWD, './packages/mall-cook-template')
+let PKG_SERVICE = resolve(CWD, './packages/mall-cook-service')
 
-const installPlatform = () =>
-  execa('yarn', ['install'], { cwd: PKG_MALL_COOK_PLATFORM })
-const installTemplate = () =>
-  execa('yarn', ['install'], { cwd: PKG_MALL_COOK_TEMPLATE })
+const installPlatform = () => execa('yarn', ['install'], { cwd: PKG_PLATFORM })
+const installTemplate = () => execa('yarn', ['install'], { cwd: PKG_TEMPLATE })
+const installService = () => execa('yarn', ['install'], { cwd: PKG_SERVICE })
 
 async function runTask (taskName, task) {
   const s = ora().start(`${taskName}平台 开始下载依赖 `)
   try {
     await task()
-    s.succeed(`${taskName}平台 下载依赖完成!`)
+    s.succeed(`${taskName}平台 下载/更新依赖完成!`)
   } catch (e) {
-    s.fail(`${taskName}平台 下载依赖失败，请到自行进入${taskName}目录下载依赖!`)
+    s.fail(`${taskName}平台 下载/更新依赖失败，请到自行进入${taskName}目录下载/更新依赖!`)
     console.error(`失败原因：${e.toString()}`)
   }
 }
@@ -32,4 +32,5 @@ async function runTask (taskName, task) {
 ;(async () => {
   await runTask('mall-cook-platform', installPlatform)
   await runTask('mall-cook-template', installTemplate)
+  await runTask('mall-cook-service', installService)
 })()
